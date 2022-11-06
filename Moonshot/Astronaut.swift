9f78be2866ec1f793f76612dde35d97005075205
Juan Gutierrez <juan.gutierrez@auro-group.com>
2022-11-06 11:26:50 +0100

    Initial commit

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Juan Gutierrez on 28/10/22.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
